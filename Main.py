e1a2abd16fe8d8a3c037d19cf4606e4ed0b3db85
Abdelrahman Elsayed <bodyomar79@gmail.com>
2023-07-18 06:54:35 +0300

    refactoring level 1
    refactor and add documentation to init function

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 from functools import partial
-
 import matplotlib
 import numpy as np
 import sys
-from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2 import QtGui, QtWidgets
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QMessageBox, QSpacerItem, \
     QLabel
@@ -12,17 +11,20 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
     NavigationToolbar2QT as NavigationToolbar
 import re
 
+# Global variables
 step = 0.1
 
 
+# here we validate the function by comparing it to a regex
 def validate_function(inp):
-    pattern = r'^(-?[0-9x]+[\+\-\*/\^])*-?[0-9x]+$'  # Regular expression pattern
+    pattern = r'^(-?[0-9x]+[\+\-\*/\^])*-?[0-9x]+$'
     if re.match(pattern, inp):
         return True
     else:
         return False
 
 
+# here we validate the start and end by comparing it to a regex
 def validate_limits(inp):
     pattern = r'^(-?[1-9][0-9]*|0)$'  # Regular expression pattern
     if re.match(pattern, inp):
@@ -30,31 +32,26 @@ def validate_limits(inp):
     else:
         return False
 
-def evaluate(self, x, function, index):
+
+# here we evaluate the y result of every point to be drawn
+def evaluate(app, x, function, index):
     try:
         fun = function.replace("^", "**")
         return eval(fun)
     except ZeroDivisionError:
-        self.alert(f"Please don't divide by zero in function {index + 1}")
+        app.alert(f"Please don't divide by zero in function {index + 1}")
         return None
     except Exception as e:
-        self.alert(str(e))
+        app.alert(str(e))
         return None
 
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+        # initialize variables
+        self.button_draw = None
         loader = QUiLoader()
-        self.setWindowTitle('Set Matplotlib Chart Value with QLineEdit Widget')
-        self.window_width, self.window_height = 1200, 800
-        self.setMinimumSize(self.window_width, self.window_height)
-
-        layout = QHBoxLayout()
-        vLayout = QVBoxLayout()
-        # layout.setContentsMargins(20,0,20,0)
-        self.setLayout(layout)
-
         self.colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'pink']
         self.input_function = []
         self.color_label = []
@@ -63,107 +60,75 @@ class MyApp(QWidget):
         self.button_add = []
         self.button_remove = []
         self.spacing = []
+        index = 0
+        
+        # set window options 
+        self.setWindowTitle('Set Matplotlib Chart Value with QLineEdit Widget')
+        self.window_width, self.window_height = 1200, 800
+        self.setMinimumSize(self.window_width, self.window_height)
 
-        self.input_function.append(QLineEdit())
-        self.input_function[0].setPlaceholderText("Function")
+        # create first function boxes
+        self.function_text_box_creation(index)
+        self.start_and_end_box_creation(index, 80)
+        self.remove_and_add_button_creation(index)
+        self.draw_button_creation()
 
-        self.color_label.append(QLabel("<font color='blue'>⬤</font>"))
-        color_font = self.color_label[0].font()
-        color_font.setPointSize(15)
-        self.color_label[0].setFont(color_font)
-        # color_font.set(True)
-
-        self.input_start.append(QLineEdit())
-        self.input_start[0].setFixedWidth(80)
-        self.input_start[0].setPlaceholderText("Start")
-
-        self.input_end.append(QLineEdit())
-        self.input_end[0].setFixedWidth(80)
-        self.input_end[0].setPlaceholderText("End")
-
-        self.button_add.append(QPushButton())
-        self.button_add[0].setText("Add")
-
-        self.button_remove.append(QPushButton())
-        self.button_remove[0].setText("X")
-        self.button_remove[0].setVisible(False)
-
-        self.button_add[0].setText("Add")
-        self.button_draw = QPushButton()
-        self.button_draw.setText("Draw")
-        self.button_draw.setFixedHeight(80)
-        font = QtGui.QFont()
-        font.setPointSize(30)
-        self.button_draw.setFont(font)
-
-        self.button_draw.clicked.connect(self.update_plot)
-        self.button_add[0].clicked.connect(self.add_function)
-
-        self.function_layout = QVBoxLayout()
-        textVLayout = QVBoxLayout()
-        colorLayout = QHBoxLayout()
-        colorLayout.addWidget(self.color_label[0])
-        colorLayout.addWidget(self.input_function[0])
-
-        textVLayout.addLayout(colorLayout)
-
-        hLayout = QHBoxLayout()
-        hLayout.addWidget(self.input_start[0])
-        hLayout.addWidget(self.input_end[0])
-        hLayout.setSpacing(15)
-
-        textVLayout.addLayout(hLayout)
-        textVLayout.addWidget(self.button_add[0])
-
-        self.function_layout.addLayout(textVLayout)
-        self.spacing.append(QSpacerItem(20, 20))
-        self.function_layout.addItem(self.spacing[0])
-
-        vLayout.addLayout(self.function_layout)
-        vLayout.addStretch()
-        vLayout.setContentsMargins(0, 40, 5, 0)
-
-        vLayout.addWidget(self.button_draw)
-        layout.addLayout(vLayout)
-
+        # create the canvas and its toolbar
         self.canvas = FigureCanvas(plt.Figure(figsize=(15, 7)))
         toolbar = NavigationToolbar(self.canvas, self)
 
-        vLayout2 = QVBoxLayout()
-        vLayout2.addWidget(toolbar)
-        vLayout2.addWidget(self.canvas)
+        # initialize the layouts
+        app_layout = QHBoxLayout()
+        side_bar_layout = QVBoxLayout()
+        self.function_layout = QVBoxLayout()
+        function_box_layout = QVBoxLayout()
+        function_text_box_layout = QHBoxLayout()
+        start_and_end_layout = QHBoxLayout()
+        main_canvas_layout = QVBoxLayout()
+        
+        # set main app layout
+        self.setLayout(app_layout)
+        
+        # layout function text box and add it to the function box layout
+        function_text_box_layout.addWidget(self.color_label[index])
+        function_text_box_layout.addWidget(self.input_function[index])
+        function_box_layout.addLayout(function_text_box_layout)
 
-        layout.addLayout(vLayout2)
+        # layout start and end text boxes and add it to the function box layout
+        start_and_end_layout.addWidget(self.input_start[index])
+        start_and_end_layout.addWidget(self.input_end[index])
+        start_and_end_layout.setSpacing(15)
+        function_box_layout.addLayout(start_and_end_layout)
+        
+        # add button to function box layout
+        function_box_layout.addWidget(self.button_add[index])
+        
+        # add the first function layout to the main function layout
+        self.function_layout.addLayout(function_box_layout)
+        self.spacing.append(QSpacerItem(20, 20))
+        self.function_layout.addItem(self.spacing[index])
+        
+        # add the main function layout and the draw button to the sidebar layout and then add it to the app layout
+        side_bar_layout.addLayout(self.function_layout)
+        side_bar_layout.addStretch()
+        side_bar_layout.setContentsMargins(0, 40, 5, 0)
+        side_bar_layout.addWidget(self.button_draw)
+        app_layout.addLayout(side_bar_layout)
 
-        self.insert_axes()
+        # add the canvas and its toolbar to the main canvas layout and then add it to the app layout
+        main_canvas_layout.addWidget(toolbar)
+        main_canvas_layout.addWidget(self.canvas)
+        app_layout.addLayout(main_canvas_layout)
 
-        # self.window=loader.load("app_v1.ui", self)
-        # self.window.MplWidget = MatplotlibWidget(self.window.MplWidget)
-        # self.window.MplWidget.setGeometry(QtCore.QRect(0, 0, self.window.MplWidget.width(),
-        #                                     self.window.MplWidget.height()))  # Set the size and position of the widget
-        # self.window.button_draw.clicked.connect(self.window.MplWidget.plotGraph)
-        # self.window.show()
-
-    def insert_axes(self):
-        font = {
-            'weight': 'normal',
-            'size': 16
-        }
-        matplotlib.rc('font', **font)
-
+        # draw the main axis
         self.axes = self.canvas.figure.subplots()
         self.axes.set_ylim([0, 100])
         self.axes.set_xlim([0, 1])
         self.plot_axes()
         self.plot = None
 
-    def add_function(self):
-        index = len(self.input_function)
-        button_add_layout = QVBoxLayout()
-        new_function_layout = QHBoxLayout()
-        textVLayout = QVBoxLayout()
 
-        self.button_add[index - 1].setVisible(False)
+    def function_text_box_creation(self, index):
         self.input_function.append(QLineEdit())
         self.input_function[index].setPlaceholderText("Function")
 
@@ -172,41 +137,70 @@ class MyApp(QWidget):
         color_font.setPointSize(15)
         self.color_label[index].setFont(color_font)
 
-        colorLayout = QHBoxLayout()
-        colorLayout.addWidget(self.color_label[index])
-        colorLayout.addWidget(self.input_function[index])
-        textVLayout.addLayout(colorLayout)
-
+    def start_and_end_box_creation(self, index, size):
         self.input_start.append(QLineEdit())
-        self.input_start[index].setFixedWidth(68)
+        self.input_start[index].setFixedWidth(size)
         self.input_start[index].setPlaceholderText("Start")
 
         self.input_end.append(QLineEdit())
-        self.input_end[index].setFixedWidth(68)
+        self.input_end[index].setFixedWidth(size)
         self.input_end[index].setPlaceholderText("End")
 
-        hLayout = QHBoxLayout()
-        hLayout.addWidget(self.input_start[index])
-        hLayout.addWidget(self.input_end[index])
-        hLayout.setSpacing(10)
-        textVLayout.addLayout(hLayout)
-
-        new_function_layout.addLayout(textVLayout)
-
+    def remove_and_add_button_creation(self, index):
         self.button_remove.append(QPushButton())
         self.button_remove[index].setText("X")
-        self.button_remove[index].setFixedWidth(20)
-        self.button_remove[index].setFixedHeight(50)
-        # self.button_remove[index].setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
-        self.button_remove[index].clicked.connect(partial(self.remove_function, index))
+        if index == 0:
+            self.button_remove[index].setVisible(False)
+        else:
+            self.button_remove[index].setFixedWidth(20)
+            self.button_remove[index].setFixedHeight(50)
+            self.button_remove[index].clicked.connect(partial(self.remove_function, index))
+
+        self.button_add.append(QPushButton())
+        self.button_add[index].setText("Add")
+        self.button_add[index].clicked.connect(self.add_function)
+
+    def draw_button_creation(self):
+        self.button_draw = QPushButton()
+        self.button_draw.setText("Draw")
+        self.button_draw.setFixedHeight(80)
+        font = QtGui.QFont()
+        font.setPointSize(30)
+        self.button_draw.setFont(font)
+        self.button_draw.clicked.connect(self.update_plot)
+
+
+    def add_function(self):
+        index = len(self.input_function)
+        button_add_layout = QVBoxLayout()
+        new_function_layout = QHBoxLayout()
+        function_box_layout = QVBoxLayout()
+
+        self.button_add[index - 1].setVisible(False)
+
+        self.function_text_box_creation(index)
+
+        function_text_box_layout = QHBoxLayout()
+        function_text_box_layout.addWidget(self.color_label[index])
+        function_text_box_layout.addWidget(self.input_function[index])
+        function_box_layout.addLayout(function_text_box_layout)
+
+        self.start_and_end_box_creation(index, 68)
+
+        start_and_end_layout = QHBoxLayout()
+        start_and_end_layout.addWidget(self.input_start[index])
+        start_and_end_layout.addWidget(self.input_end[index])
+        start_and_end_layout.setSpacing(10)
+        function_box_layout.addLayout(start_and_end_layout)
+
+        new_function_layout.addLayout(function_box_layout)
+
+        self.remove_and_add_button_creation(index)
 
         new_function_layout.addWidget(self.button_remove[index])
 
         button_add_layout.addLayout(new_function_layout)
 
-        self.button_add.append(QPushButton())
-        self.button_add[index].setText("Add")
-        self.button_add[index].clicked.connect(self.add_function)
         button_add_layout.addWidget(self.button_add[index])
         if len(self.input_function) == 8:
             self.button_add[index].setVisible(False)
@@ -260,7 +254,7 @@ class MyApp(QWidget):
             function = self.input_function[i].text()
             start = self.input_start[i].text()
             end = self.input_end[i].text()
-            if not self.validate( start, end, function, i,True):
+            if not self.validate(start, end, function, i, True):
                 self.canvas.draw()
                 return
             x, y = self.points_maker(int(start), int(end), function, i)
@@ -327,7 +321,7 @@ class MyApp(QWidget):
         y_res = []
         for i in np.arange(start, end + step, step):
             x_res.append(round(i, 3))
-            temp = evaluate(self,i, function, index)
+            temp = evaluate(self, i, function, index)
             if temp != None:
                 y_res.append(round(temp, 3))
             else:
@@ -341,30 +335,30 @@ class MyApp(QWidget):
         self.dlg.setText(message)
         button = self.dlg.exec_()
 
-    def validate(self, start, end, function, index,alertbool):
+    def validate(self, start, end, function, index, alertbool):
         if not validate_function(function):
-            if alertbool :
+            if alertbool:
                 self.alert(
                     f"function {index + 1} should be consists of numbers and x's \nand between each two only one of these +,-,*,/,^")
             return False
         if start == "":
-            if alertbool :
+            if alertbool:
                 self.alert(f"Please Enter start point for function {index + 1}")
             return False
         if end == "":
-            if alertbool :
+            if alertbool:
                 self.alert(f"Please Enter start point for function {index + 1}")
             return False
         if not validate_limits(start):
-            if alertbool :
+            if alertbool:
                 self.alert(f"Start point of function {index + 1} should be a number")
             return False
         if not validate_limits(end):
-            if alertbool :
+            if alertbool:
                 self.alert(f"End point of function {index + 1} should be a number")
             return False
         if float(start) > float(end):
-            if alertbool :
+            if alertbool:
                 self.alert(f"Start point should be less than or equal \nthe end point for function {index + 1}")
             return False
         return True
